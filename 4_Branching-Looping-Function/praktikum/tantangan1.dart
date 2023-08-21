695fb6fb72e9bfcbea3c48dbebd0bcecc2f3cedf
sadewaammar21 <sadewaammars47t@gmail.com>
2023-08-21 20:31:10 +0700

    tantangan1 materi 4_Branching-Looping-Function

---
+++ 4_Branching-Looping-Function/praktikum/tantangan1.dart
@@ -0,0 +1,17 @@
+var buku = 10000;
+var pensil = 5000;
+var tas = 100000;
+int jumlahHargaBarang(buku, pensil, tas) {
+  return buku + pensil + tas;
+}
+
+void main() {
+  print(jumlahHargaBarang(buku, pensil, tas));
+}
+
+//1. buatlah  function untuk  menghitung jumlah barang yang  kita  beli  dari sebuah toko
+//var buku = 10000;
+//var pensil = 5000;
+//var tas = 100000;
+//2.  hitunglah  diskon 10%  dari  jumlah  
+//3. hitunglah  harga yang harus di bayar oleh kita
